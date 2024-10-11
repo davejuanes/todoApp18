d9728a2c052f2c048704cbc55a750fb2b2d7aa10
DAVID MERCADO JUANES <dmercado@abc.gob.bo>
2024-10-10 22:29:24 -0400

    [CLASS 19] FIRST COMMIT

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,91 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
+
+import { Task } from "./../../models/task.model";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Instalar Angular CLI',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componentes',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear servicios',
+      completed: false
+    },
+  ]);
+
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      // Validators.pattern()
+    ]
+  }); // El primer valor es el por defecto
+
+  changeHandler() {
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== '') {
+        this.addTask(value)
+        this.newTaskCtrl.setValue('') // Deja el inpuit en vacio cuando se guarda una tarea
+      }
+    }
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title: title,
+      completed: false
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index))
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
+  }
+
+  toogleChekedTask(index: number) {
+    this.tasks.update((tasks) => tasks.map((task, position) => {
+      if (position === index) {
+        task.completed = !task.completed;
+      }
+      return task;
+    }));
+  }
+}
---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,69 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css',
+})
+export class LabsComponent {
+  welcome = 'Hola bienvenido!';
+  tasks = signal(['Instalar Angular CLI', 'Crear proyecto', 'Crear componentes', 'Crear servicios']);
+  name = signal('Dave');
+  age = 33;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+
+  person = signal({
+    name: 'Dave',
+    age: 33,
+    img: 'https://w3schools.com/howto/img_avatar.png',
+  });
+  colorCtrl = new FormControl();
+  widthCtrl = new FormControl(50, {
+    nonNullable: true
+  });
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe(value => {
+      console.log(value);
+    })
+  }
+
+  clickHandler() {
+    alert('Hola');
+  }
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value
+    this.name.set(newValue)
+  }
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+
+  changeAge(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value
+    this.person.update(prevState => {
+      return {
+        ...prevState,
+        age: parseInt(newValue, 10)
+      }
+    })
+  }
+  changeName(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value
+    this.person.update(prevState => {
+      return {
+        ...prevState,
+        name: newValue
+      }
+    })
+  }
+}
